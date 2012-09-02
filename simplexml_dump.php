853249c2841c1ba9a2b1ee84523a94607dcb9e4f
Rowan Collins <git@rwec.co.uk>
2012-09-02 21:42:18 +0100

    Improve comments, add return-mode, remove test code

--- simplexml_dump.php
+++ simplexml_dump.php
@@ -1,6 +1,15 @@
 <?php
 
-function simplexml_dump(SimpleXMLElement $sxml)
+/**
+ * Output a summary of the node or list of nodes referenced by a particular SimpleXML object
+ * Rather than attempting a recursive inspection, presents statistics aimed at understanding
+ * what your SimpleXML code is doing.
+ *
+ * @param SimpleXMLElement $sxml The object to inspect
+ * @param boolean $return Default false. If true, return the "dumped" info rather than echoing it
+ * @return null|string Nothing, or output, depending on $return param
+ */
+function simplexml_dump(SimpleXMLElement $sxml, $return=false)
 {
 	$indent = "\t";
 
@@ -8,6 +17,10 @@ function simplexml_dump(SimpleXMLElement $sxml)
 	// Note that the header is added at the end, so we can add stats
 	$dump .= '[' . PHP_EOL;
 
+	// SimpleXML objects can be either a single node, or (more commonly) a list of 0 or more nodes
+	// I haven't found a reliable way of distinguishing between the two cases
+	// Note that for a single node, foreach($node) acts like foreach($node->children())
+	// Numeric array indexes, however, operate consistently: $node[0] just returns the node
 	$item_index = 0;
 	while ( isset($sxml[$item_index]) )
 	{
@@ -16,26 +29,12 @@ function simplexml_dump(SimpleXMLElement $sxml)
 
 		// It's surprisingly hard to find something which behaves consistently differently for an attribute and an element within SimpleXML
 		// The below relies on the fact that the DOM makes a much clearer distinction
+		// Note that this is not an expensive conversion, as we are only swapping PHP wrappers around an existing LibXML resource
 		if ( dom_import_simplexml($item) instanceOf DOMAttr )
 		{
 			$dump .= $indent . 'Attribute {' . PHP_EOL;
 
-			$ns = $item->getNamespaces(false);
-			if ( $ns )
-			{
-				$dump .= $indent . $indent . 'Namespace: \'' . reset($ns) . '\'' . PHP_EOL;
-				$dump .= $indent . $indent . 'Namespace Alias: \'' . key($ns) . '\'' . PHP_EOL;
-			}
-
-			$dump .= $indent . $indent . 'Name: \'' . $item->getName() . '\'' . PHP_EOL;
-			$dump .= $indent . $indent . 'Value: \'' . (string)$item . '\'' . PHP_EOL;
-
-			$dump .= $indent . '}' . PHP_EOL;
-		}
-		else
-		{
-			$dump .= $indent . 'Element {' . PHP_EOL;
-
+			// To what namespace does this attribute belong? Returns array( alias => URI )
 			$ns = $item->getNamespaces(false);
 			if ( $ns )
 			{
@@ -51,14 +50,41 @@ function simplexml_dump(SimpleXMLElement $sxml)
 			}
 
 			$dump .= $indent . $indent . 'Name: \'' . $item->getName() . '\'' . PHP_EOL;
+			$dump .= $indent . $indent . 'Value: \'' . (string)$item . '\'' . PHP_EOL;
+
+			$dump .= $indent . '}' . PHP_EOL;
+		}
+		else
+		{
+			$dump .= $indent . 'Element {' . PHP_EOL;
+
+			// To what namespace does this element belong? Returns array( alias => URI )
+			$ns = $item->getNamespaces(false);
+			if ( $ns )
+			{
+				$dump .= $indent . $indent . 'Namespace: \'' . reset($ns) . '\'' . PHP_EOL;
+				if ( key($ns) == '' )
+				{
+					$dump .= $indent . $indent . '(Default Namespace)' . PHP_EOL;
+				}
+				else
+				{
+					$dump .= $indent . $indent . 'Namespace Alias: \'' . key($ns) . '\'' . PHP_EOL;
+				}
+			}
+
+			$dump .= $indent . $indent . 'Name: \'' . $item->getName() . '\'' . PHP_EOL;
+			// REMEMBER: ALWAYS CAST TO STRING! :)
 			$dump .= $indent . $indent . 'String Content: \'' . (string)$item . '\'' . PHP_EOL;
 
 			// Now some statistics about attributes and children, by namespace
 
+			// This returns all namespaces used by this node and all its descendants,
+			// 	whether declared in this node, in its ancestors, or in its descendants
 			$all_ns = $item->getNamespaces(true);
 			// If the default namespace is never declared, it will never show up using the below code
 			// This will still mess up in the case where a parent element is missing the xmlns declaration,
-			//	but a child adds it, because SimpleXML will look ahead and fill $all_ns[''] incorrectly
+			//	but a descendant adds it, because SimpleXML will look ahead and fill $all_ns[''] incorrectly
 			if ( ! array_key_exists('', $all_ns) )
 			{
 				$all_ns[''] = NULL;
@@ -94,31 +120,12 @@ function simplexml_dump(SimpleXMLElement $sxml)
 	// Add on the header line, with the total number of items output
 	$dump = 'SimpleXML object (' . $item_index . ' item' . ($item_index > 1 ? 's' : '') . ')' . PHP_EOL . $dump;
 
-	echo $dump;
+	if ( $return )
+	{
+		return $dump;
+	}
+	else
+	{
+		echo $dump;
+	}
 }
-
-$x = <<<XML
-<?xml version="1.0" encoding="utf-8"?>
-<Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-  <loofah />
-  <soapenv:Body>
-    <requestContactResponse xmlns="http://webservice.foo.com">
-      <requestContactReturn>
-        <errorCode xsi:nil="true"/>
-        <errorDesc xsi:nil="true"/>
-        <id>744</id>
-        <soapenv:id>744</soapenv:id>
-      </requestContactReturn>
-    </requestContactResponse>
-  </soapenv:Body>
-</Envelope>
-XML;
-
-$sx = simplexml_load_string($x);
-
-simplexml_dump($sx);
-simplexml_dump($sx->children(NULL));
-simplexml_dump($sx->children("soapenv", true)->Body);
-simplexml_dump($sx->children("soapenv", true)->Body->children(NULL)->requestContactResponse->requestContactReturn->id);
-simplexml_dump($sx->children("soapenv", true)->Body->children(NULL)->requestContactResponse->requestContactReturn->children('soapenv', true)->id);
-
