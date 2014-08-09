af30ba47b689b9172b04cdb3871d9f2142a12373
Josh Sherman <josh@gravityblvd.com>
2014-08-08 22:16:54 -0400

    Working on better code coverage
    Added a new test that gets us up to 80% coverage. Still a ways to go.

--- tests/simplexml_dump_Test.php
+++ tests/simplexml_dump_Test.php
@@ -57,6 +57,27 @@ class simplexml_dump_Test extends simplexml_dump_bootstrap
 		$return = simplexml_dump($this->simpleXML_NS, true);
 		$this->assertEquals($this->expected_NS, $return);
 	}
+
+	public function testDumpWithSingleNodeWithAttributesAndNS()
+	{
+		$xml  = '<parent xmlns:ns="ns"><ns:child ns:foo="bar" /></parent>';
+		$sxml = simplexml_load_string($xml);
+
+		$return = simplexml_dump($sxml->children('ns', true)->child->attributes('ns'), true);
+
+		$expected = "SimpleXML object (1 item)
+[
+	Attribute {
+		Namespace: 'ns'
+		Namespace Alias: 'ns'
+		Name: 'foo'
+		Value: 'bar'
+	}
+]
+";
+
+		$this->assertEquals($expected, $return);
+	}
 }
 
 ?>
