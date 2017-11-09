60afe7f7b9ea21336fec34e78675a260b92f2d19
Rowan Collins <git@rwec.co.uk>
2017-11-09 20:02:34 +0000

    Rearrange tests to be a directory of samples, all tested the same way

--- tests/simplexml_dump_Test.php
+++ tests/simplexml_dump_Test.php
@@ -1,131 +1,34 @@
 <?php
 
-class simplexml_dump_Test extends simplexml_dump_bootstrap
+class simplexml_dump_test extends PHPUnit_Framework_TestCase
 {
-	public function setUp()
-	{
-		$this->expected = "SimpleXML object (1 item)
-[
-	Element {
-		Name: 'movies'
-		String Content: '
-				
-			'
-		Content in Default Namespace
-			Children: 1 - 1 'movie'
-			Attributes: 0
-	}
-]
-";
+    public function loadExamples()
+    {
+        $test_cases = array();
 
-		$this->expected_default_NS = "SimpleXML object (1 item)
-[
-	Element {
-		Namespace: 'https://github.com/IMSoP/simplexml_debug'
-		(Default Namespace)
-		Name: 'movies'
-		String Content: '
-				
-			'
-		Content in Default Namespace
-			Namespace URI: 'https://github.com/IMSoP/simplexml_debug'
-			Children: 1 - 1 'movie'
-			Attributes: 0
-	}
-]
-";
+        foreach ( new DirectoryIterator(__DIR__ . '/dump-output') as $fileInfo )
+        {
+            if($fileInfo->isDot()) {
+                continue;
+            }
 
-		$this->expected_named_NS = "SimpleXML object (1 item)
-[
-	Element {
-		Name: 'movies'
-		String Content: '
-				
-			'
-		Content in Namespace test
-			Namespace URI: 'https://github.com/IMSoP/simplexml_debug'
-			Children: 1 - 1 'movie'
-			Attributes: 0
-	}
-]
-";
+            $filename = $fileInfo->getFilename();
 
-		parent::setUp();
-	}
+            $test_cases[$filename] = array(
+                file_get_contents(__DIR__ . '/input/' . $filename . '.xml'),
+                file_get_contents(__DIR__ . '/dump-output/' . $filename)
+            );
+        }
 
-	public function testDump()
-	{
-		ob_start();
-		simplexml_dump($this->simpleXML);
-		$return = ob_get_contents();
-		ob_end_clean();
+        return $test_cases;
+    }
 
-		$this->assertEquals($this->expected, $return);
-	}
-
-	public function testDumpReturn()
-	{
-		$return = simplexml_dump($this->simpleXML, true);
-		$this->assertEquals($this->expected, $return);
-	}
-
-	public function testDumpWithDefaultNS()
-	{
-		$return = simplexml_dump($this->simpleXML_default_NS, true);
-		$this->assertEquals($this->expected_default_NS, $return);
-	}
-
-	public function testDumpWithNamedNS()
-	{
-		$return = simplexml_dump($this->simpleXML_named_NS, true);
-		$this->assertEquals($this->expected_named_NS, $return);
-	}
-
-	public function testDumpAttributeWithNamedNS()
-	{
-		$xml  = '<parent xmlns:ns="ns"><ns:child ns:foo="bar" /></parent>';
-		$sxml = simplexml_load_string($xml);
-
-		$return = simplexml_dump($sxml->children('ns', true)->child->attributes('ns'), true);
-
-		$expected = "SimpleXML object (1 item)
-[
-	Attribute {
-		Namespace: 'ns'
-		Namespace Alias: 'ns'
-		Name: 'foo'
-		Value: 'bar'
-	}
-]
-";
-
-		$this->assertEquals($expected, $return);
-	}
-
-	public function testDumpMultipleAttributes()
-	{
-		$xml  = '<parent xmlns:ns="ns"><child ns:one="1" ns:two="2" ns:three="3" /></parent>';
-		$sxml = simplexml_load_string($xml);
-
-		$return = simplexml_dump($sxml->child, true);
-
-		$expected = "SimpleXML object (1 item)
-[
-	Element {
-		Namespace: 'ns'
-		Namespace Alias: 'ns'
-		Name: 'child'
-		String Content: ''
-		Content in Namespace ns
-			Namespace URI: 'ns'
-			Children: 0
-			Attributes: 3 - 'one', 'two', 'three'
-	}
-]
-";
-
-		$this->assertEquals($expected, $return);
-	}
+    /**
+     * @dataProvider loadExamples
+     */
+    public function testDumpReturn($xml, $expected_output)
+    {
+        $return = simplexml_dump(simplexml_load_string($xml), true);
+        $this->assertSame($expected_output, $return);
+    }
 }
-
-?>
--- tests/simplexml_tree_Test.php
+++ tests/simplexml_tree_Test.php
@@ -1,149 +1,34 @@
 <?php
 
-class simplexml_tree_Test extends simplexml_dump_bootstrap
+class simplexml_tree_test extends PHPUnit_Framework_TestCase
 {
-	public function setUp()
-	{
-		$this->expected = "SimpleXML object (1 item)
-[0] // <movies>
-	->movie[0]
-		->title[0]
-		->characters[0]
-			->character[0]
-				->name[0]
-				->actor[0]
-			->character[1]
-				->name[0]
-				->actor[0]
-		->plot[0]
-		->great-lines[0]
-			->line[0]
-		->rating[0]
-			['type']
-		->rating[1]
-			['type']
-";
+    public function loadExamples()
+    {
+        $test_cases = array();
 
-		$this->expected_default_NS = "SimpleXML object (1 item)
-[0] // <movies>
-	->movie[0]
-		->title[0]
-		->characters[0]
-			->character[0]
-				->name[0]
-				->actor[0]
-			->character[1]
-				->name[0]
-				->actor[0]
-		->plot[0]
-		->great-lines[0]
-			->line[0]
-		->rating[0]
-			['type']
-		->rating[1]
-			['type']
-";
+        foreach ( new DirectoryIterator(__DIR__ . '/dump-output') as $fileInfo )
+        {
+            if($fileInfo->isDot()) {
+                continue;
+            }
 
-		$this->expected_named_NS = "SimpleXML object (1 item)
-[0] // <movies>
-	->children('test', true)
-		->movie[0]
-			->title[0]
-			->characters[0]
-				->character[0]
-					->name[0]
-					->actor[0]
-				->character[1]
-					->name[0]
-					->actor[0]
-			->plot[0]
-			->great-lines[0]
-				->line[0]
-			->rating[0]
-				->attributes('', true)
-					->type
-			->rating[1]
-				->attributes('', true)
-					->type
-";
+            $filename = $fileInfo->getFilename();
 
-		parent::setUp();
-	}
+            $test_cases[$filename] = array(
+                file_get_contents(__DIR__ . '/input/' . $filename . '.xml'),
+                file_get_contents(__DIR__ . '/tree-output/' . $filename)
+            );
+        }
 
-	public function testTree()
-	{
-		ob_start();
-		simplexml_tree($this->simpleXML);
-		$return = ob_get_contents();
-		ob_end_clean();
+        return $test_cases;
+    }
 
-		$this->assertEquals($this->expected, $return);
-	}
-
-	public function testTreeIncludeStringContent()
-	{
-		ob_start();
-		simplexml_tree($this->simpleXML, true);
-		$return = ob_get_contents();
-		ob_end_clean();
-
-		$expected = "SimpleXML object (1 item)
-[0] // <movies>
-	(string) '' (9 chars)
-	->movie[0]
-		(string) '' (41 chars)
-		->title[0]
-			(string) 'PHP: Behind the...' (22 chars)
-		->characters[0]
-			(string) '' (20 chars)
-			->character[0]
-				(string) '' (23 chars)
-				->name[0]
-					(string) 'Ms. Coder' (9 chars)
-				->actor[0]
-					(string) 'Onlivia Actora' (14 chars)
-			->character[1]
-				(string) '' (23 chars)
-				->name[0]
-					(string) 'Mr. Coder' (9 chars)
-				->actor[0]
-					(string) 'El ActÓr' (9 chars)
-		->plot[0]
-			(string) 'So, this langua...' (174 chars)
-		->great-lines[0]
-			(string) '' (13 chars)
-			->line[0]
-				(string) 'PHP solves all ...' (30 chars)
-		->rating[0]
-			(string) '7' (1 chars)
-			['type']
-				(string) 'thumbs' (6 chars)
-		->rating[1]
-			(string) '5' (1 chars)
-			['type']
-				(string) 'stars' (5 chars)
-";
-
-		$this->assertEquals($expected, $return);
-	}
-
-	public function testTreeReturn()
-	{
-		$return = simplexml_tree($this->simpleXML, false, true);
-		$this->assertEquals($this->expected, $return);
-	}
-
-	public function testTreeWithDefaultNS()
-	{
-		$return = simplexml_tree($this->simpleXML_default_NS, false, true);
-		$this->assertEquals($this->expected_default_NS, $return);
-	}
-
-	public function testTreeWithNamedNS()
-	{
-		$return = simplexml_tree($this->simpleXML_named_NS, false, true);
-		$this->assertEquals($this->expected_named_NS, $return);
-	}
+    /**
+     * @dataProvider loadExamples
+     */
+    public function testTreeReturn($xml, $expected_output)
+    {
+        $return = simplexml_tree(simplexml_load_string($xml), false, true);
+        $this->assertSame($expected_output, $return);
+    }
 }
-
-?>
